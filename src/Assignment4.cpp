c6f5c52c788e650f3a8e8411b19cdf687430eb01
omarhaassaan <omarhassan321@hotmail.com>
2020-08-12 17:34:19 +0200

    Calculator with addition and subtraction

---
+++ src/Assignment4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char op;
+	cout << "Enter the operation you want (+/-):" <<endl;
+	cin >> op;
+	int num1, num2, result;
+	cout << "Enter two Digits" << endl;
+	cin >> num1 >> num2;
+	switch (op) {
+	case '+':
+		result = num1 + num2;
+		cout << num1<<"+"<<num2<<" = "<<result;
+		break;
+	case '-':
+		result = num1 - num2;
+		cout << num1<<"-"<<num2<<" = "<<result;
+		break;
+	default:
+		cout << "wrong Operation" <<endl;
+	}
+
+	return 0;
+}
